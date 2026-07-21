9a78e3fab512e1e270fd4216005377eafbcfd37f
Katarína Cepková <cepkovakatarina3@gmail.com>
2026-07-21 19:31:39 +0200

    Write contract for requeueing policies

---
+++ Flashcards.Core/Learning/IRequeuePolicy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flashcards.Core.Learning
+{
+    public interface IRequeuePolicy
+    {
+        public bool ShouldRemove(LearningSessionCard card);
+
+        public uint StepsAhead(LearningSessionCard card);
+    }
+}
