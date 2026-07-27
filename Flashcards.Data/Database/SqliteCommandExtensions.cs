5802becdebcfa37f5f03af8643073bf61055c755
Katarína Cepková <cepkovakatarina3@gmail.com>
2026-07-27 14:18:45 +0200

    Fix bug in namespace

--- Flashcards.Data/Database/SqliteCommandExtensions.cs
+++ Flashcards.Data/Database/SqliteCommandExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Data.Sqlite;
 
-namespace Flashcards.Data.Database
+namespace Flashcards.Data
 {
     /// <summary>
     /// Binds C# values as SQLite command parameters, converting types (dates, nulls)
