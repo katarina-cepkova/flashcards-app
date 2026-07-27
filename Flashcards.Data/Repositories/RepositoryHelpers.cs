c9461b20f097bff37f29ded219d632bae9264656
Katarína Cepková <cepkovakatarina3@gmail.com>
2026-07-27 18:07:23 +0200

    Move common functionality to a static class to be accessible by flashcard repository later

---
+++ Flashcards.Data/Repositories/RepositoryHelpers.cs
@@ -0,0 +1,21 @@
+﻿using Flashcards.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flashcards.Data.Repositories
+{
+    internal static class RepositoryHelpers
+    {
+        /// <summary>
+        /// Throws <see cref="EntityNotFoundException"/> if no row was affected by the last command.
+        /// </summary>
+        public static void EnsureRowsAffected(int rowsAffected, long id)
+        {
+            if (rowsAffected == 0)
+            {
+                throw new EntityNotFoundException($"Topic {id} does not exist.");
+            }
+        }
+    }
+}
