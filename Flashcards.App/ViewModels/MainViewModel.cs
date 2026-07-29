3e2bbcb19f32dcfa8a21aaec55c622f28b1ed53c
Katarína Cepková <cepkovakatarina3@gmail.com>
2026-07-29 23:21:26 +0200

    Replace 4 separate 'OneWay' properties with one, bind converter to radio buttons

---
+++ Flashcards.App/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Input;
+using Flashcards.App.Commands;
+using Flashcards.App.Services;
+
+
+namespace Flashcards.App.ViewModels
+{
+    /// <summary>
+    /// View model for the main application window.
+    /// </summary>
+    class MainViewModel : ViewModelBase
+    {
+        private readonly ILocalizationService _localizationService;
+        private string _currentLanguage = "en-GB";  // default language at the app's start
+        public string CurrentLanguage
+        {
+            get => _currentLanguage;
+            set
+            { 
+                if (_currentLanguage == value)
+                    return;
+                _currentLanguage = value;
+                OnPropertyChanged();  // notify all subscribing UI elements
+                _localizationService.SetLanguage(value);  // change the language of the app
+                Console.WriteLine(_currentLanguage);
+            }
+        }
+
+
+        public MainViewModel(ILocalizationService localizationService)
+        {
+            _localizationService = localizationService;
+
+        }
+
+        
+    }
+}
