c17621522e0b5d21552ce2d8c39189ac4fdcf1ee
Katarína Cepková <cepkovakatarina3@gmail.com>
2026-07-30 22:02:15 +0200

    Represent app's state as an enum

---
+++ Flashcards.App/Models/AppState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flashcards.App.Models
+{
+    public enum AppState
+    {
+        ClosedSet,
+        OpenedSetView,
+        OpenedSetEdit,
+        LearningSession
+
+    }
+}
